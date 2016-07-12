011f8ac9c37a9ba31c33eca09f197acf41e9516b
tony un <tony un>
2016-07-12 09:01:22 -0400

    committing test

--- issues_test.rb
+++ issues_test.rb
@@ -1,3 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './issues'
+
+
+class test_GithubRepo
+  def perform_requests(args)
+    File.read("./test/create_repo.json")
+
+
+  def str.read_body
+    self
+  end
+  str
+end
+end
+
+
+class test_Repocreator < Minitest::test
+
+  def we_can_create_repo
+    maker = GithubRepo.new("waffle_son")
+    payload = maker.create_repo_now
+    assert_equal "waffle_son", payload ["name"]
+  end
+end
+
 
 
 http = NET::HTTP.new(uri.host, uri.port)
